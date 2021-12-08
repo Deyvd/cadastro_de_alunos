5226a01114608c0158fb6f23175c21a8ffe84b90
Marcos Calixto <marcos.calixto@huntercar.app>
2021-12-08 11:00:51 -0300

    finalizado o caminho de cadatro de um usuário para testes nas controller Usuário

--- sistema_turminha_da_musica/routes/api.php
+++ sistema_turminha_da_musica/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('usuario', Usuario::class);
 
 
 Route::group([
